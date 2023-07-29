8880fa040b9736ce70944c381e35f4bfeea120dc
Alagesan-S-R <128414693+Alagesan-S-R@users.noreply.github.com>
2023-07-29 20:13:30 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,28 @@
+var owl = $('.test');
+owl.owlCarousel({
+    loop:true,
+    margin:10,
+    autoplay:true,
+    autoplayTimeout:2000,
+    autoplayHoverPause:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },            
+        960:{
+            items:1
+        },
+        1200:{
+            items:2
+        }
+    }
+});
+$('.play').on('click',function(){
+    owl.trigger('play.owl.autoplay',[1000])
+})
+$('.stop').on('click',function(){
+    owl.trigger('stop.owl.autoplay')
+})
